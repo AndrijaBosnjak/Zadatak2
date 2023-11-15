5887fc969e679ce45435df64b3e4dca221ecb4c9
AndrijaBosnjak <andrijab2@gmail.com>
2023-11-15 20:25:21 +0100

    add Questions component

---
+++ src/components/questions/Questions.jsx
@@ -0,0 +1,54 @@
+const Questions = () => [
+    {
+      numberA: 123, 
+      numberB: 184,
+      sum: 307  
+    },
+    {
+      numberA: 214, 
+      numberB: 107,
+      sum: 321  
+    },
+    {
+      numberA: 214, 
+      numberB: 243,
+      sum: 457  
+    },
+    {
+      numberA: 248, 
+      numberB: 354,
+      sum: 602  
+    },
+    {
+      numberA: 352, 
+      numberB: 124,
+      sum: 476  
+    },
+    {
+      numberA: 286, 
+      numberB: 408,
+      sum: 694  
+    },
+    {
+      numberA: 385, 
+      numberB: 426,
+      sum: 811  
+    },
+    {
+      numberA: 252, 
+      numberB: 464,
+      sum: 716  
+    },
+    {
+      numberA: 105, 
+      numberB: 484,
+      sum: 589  
+    },
+    {
+      numberA: 314, 
+      numberB: 529,
+      sum: 843  
+    }
+]
+
+export default Questions;
